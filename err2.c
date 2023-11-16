251a8cd326f4cc3bd50de26e335e73650a47690f
MariaRamzy <maria_botros94@hotmail.com>
2023-11-16 08:19:41 -0800

    betty style

--- err2.c
+++ err2.c
@@ -1,15 +1,15 @@
 #include "monty.h"
 
-
 /**
  * stderr_usage - usage error.
  * Return: EXIT_FAILURE
  */
 void stderr_usage(void)
 {
-	fprintf(stderr, "USAGE: monty file\n");
-	exit(EXIT_FAILURE);
+fprintf(stderr, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
 }
+
 /**
  * stderr_malloc - Prints malloc error messages.
  * Return: EXIT_FAILURE
@@ -17,30 +17,34 @@ void stderr_usage(void)
 
 void stderr_malloc(void)
 {
-	fprintf(stderr, "Error: malloc failed\n");
-	free_globalvars();
-	exit(EXIT_FAILURE);
+fprintf(stderr, "Error: malloc failed\n");
+free_globalvars();
+exit(EXIT_FAILURE);
 }
+
 /**
  * stderr_fopen - error of fopen.
  * @filename: type pointer char of filename
  * Return: EXIT_FAILURE
  */
+
 void stderr_fopen(char *filename)
 {
-	fprintf(stderr, "Error: Can't open file %s\n", filename);
-	exit(EXIT_FAILURE);
+fprintf(stderr, "Error: Can't open file %s\n", filename);
+exit(EXIT_FAILURE);
 }
+
 /**
  * stderr_int - error of int.
  * @line_number: type pointer of line number
  * Return: EXIT_FAILURE
  */
+
 void stderr_int(unsigned int line_number)
 {
-	fprintf(stderr, "L%u: usage: push integer\n", line_number);
-	free_globalvars();
-	exit(EXIT_FAILURE);
+fprintf(stderr, "L%u: usage: push integer\n", line_number);
+free_globalvars();
+exit(EXIT_FAILURE);
 }
 
 /**
@@ -49,9 +53,10 @@ void stderr_int(unsigned int line_number)
  * @token: type pointer char of the instruction.
  * Return: EXIT_FAILURE
  */
+
 void stderr_unknown(char *token, unsigned int line_number)
 {
-	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
-	free_globalvars();
-	exit(EXIT_FAILURE);
+fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
+free_globalvars();
+exit(EXIT_FAILURE);
 }
